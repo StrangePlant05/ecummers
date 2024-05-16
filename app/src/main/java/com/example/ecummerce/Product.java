8c941246f705b9988d1c931b1e93d62c2c7bc277
= <iamzeth05@gmail.com>
2024-05-16 21:30:10 +0800

    Classes unfinished

---
+++ app/src/main/java/com/example/ecummerce/Product.java
@@ -0,0 +1,11 @@
+package com.example.ecummerce;
+
+public class Product {
+    private String id;
+    private String productName;
+    private String productDescription;
+    private String seller;
+    private double price;
+    private int quantity;
+    private int stock;
+}
---
+++ app/src/main/java/com/example/ecummerce/User.java
@@ -0,0 +1,13 @@
+package com.example.ecummerce;
+
+public class User {
+    private String id;
+    private String username;
+    private String email;
+    private String password;
+    private String mobileNumber;
+    private String fullName;
+    private String address;
+    private int age;
+
+}
