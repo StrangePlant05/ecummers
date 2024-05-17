a66834fef38ad0c75f1001c2eed5001f59276117
StrangePlant05 <iamzeth05@gmail.com>
2024-05-17 14:17:37 +0800

    cum

--- app/src/main/java/com/example/ecummerce/DisplayBeforePurchase.java
+++ app/src/main/java/com/example/ecummerce/DisplayBeforePurchase.java
@@ -1,6 +1,9 @@
 package com.example.ecummerce;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import android.os.Bundle;
 
@@ -11,7 +14,14 @@ public class DisplayBeforePurchase extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_display_before_purchase);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
 
         // TODO: set  values for current user and current product mayhaps from id from getIntent().getExtra()
 
